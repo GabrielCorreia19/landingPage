face95ea0d70853756691ef0d6da51fd6a76e8a4
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-06-10 18:39:16 -0300

    Restando apenas implementar o footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { BenefitComponent } from '../benefit/benefit.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [
+    HeaderComponent,
+    NgOptimizedImage,
+    BtnPrimaryComponent,
+    NewsletterFormComponent,
+    BenefitComponent,
+  ],
 })
 export class HomeComponent {}
